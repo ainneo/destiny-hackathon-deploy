3b92bf83bbd6abb777b415b449c86099bb5edd21
Gideon Tong <gideon@gideontong.com>
2021-02-03 13:56:04 -0800

    Create basic pets API

--- backend/webserver/__init__.py
+++ backend/webserver/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+name = 'DestinyAPI'
+version = '1.0'
 app = Flask(__name__)
 
 from webserver import routes
--- backend/webserver/routes.py
+++ backend/webserver/routes.py
@@ -1,6 +1,29 @@
-from webserver import app
+from flask import jsonify, request
+from random import randint, sample
+from json import load
+from webserver import app, name, version
 
+data_folder = '../data'
+pets = load(open(f'{data_folder}/master.json'))['count']
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return jsonify(name=name, version=version)
+
+@app.route('/pet')
+def get_random_pet():
+    selection = randint(0, pets - 1)
+    data = load(open(f'{data_folder}/data_{str(selection)}.json'))
+    return jsonify(sample(data, 1)[0])
+
+
+@app.route('/pets')
+def get_random_pets():
+    count = 5
+    try:
+        count = int(request.args['count'])
+    except:
+        pass
+    selection = randint(0, pets - 1)
+    data = load(open(f'{data_folder}/data_{str(selection)}.json'))
+    return jsonify(sample(data, count))
