0d4d921636e0016bd64462c97803c1ae547cb4ca
JacobK233811 <66180976+JacobK233811@users.noreply.github.com>
2021-02-03 13:54:18 -0500

    Passions -> Google Image
    In order to maintain the same aesthetic between our pets and passions, gets the first google images result for each passion.
    
    Note, a lot of these are pretty small and we might have to adjust the scrape to take something later than the first result or only accept a larger image.

--- scripts/scrape_designepiclife.py
+++ scripts/scrape_designepiclife.py
@@ -11,11 +11,19 @@ ordered_lists = [ol for ol in soup.findAll("ol")]
 passions = []
 for ord_lst in ordered_lists:
     passions.append([item.text for item in ord_lst.findAll("li")])
-print(passions)
+# print(passions)
 
 category_passions = {}
 for cat, pas in zip(categories, passions):
-    category_passions[cat] = pas
+    pas_dict_list = []
+    for p in pas:
+        pas_dict = {}
+        pas_dict["activity"] = p
+        ggl_imgs = get(f"https://www.google.com/search?hl=en&authuser=0&tbm=isch&source=hp&biw=1368&bih=773&ei=rOsaYMSqLuThxgGhhZWIAg&q={p}&oq={p}&gs_lcp=CgNpbWcQAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQM6AggAOggIABCxAxCDAVDGPFjJS2CzT2gBcAB4AIABR4gByQOSAQE3mAEAoAEBqgELZ3dzLXdpei1pbWewAQA&sclient=img&ved=0ahUKEwjEsPO0rM7uAhXksDEKHaFCBSEQ4dUDCAc&uact=5")
+        ggl_soup = BS(ggl_imgs.content, 'html.parser')
+        pas_dict["image"] = ggl_soup.findAll("img")[1].attrs["src"]
+        pas_dict_list.append(pas_dict)
+    category_passions[cat] = pas_dict_list
 
 with open("../data/passions.json", 'w+') as output_file:
     output_file.write(json.dumps(category_passions, indent=4))
